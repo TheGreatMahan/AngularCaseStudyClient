570701a9f2181a0dfa510ea194b83318d5799b38
Mahan <mahanmahdipour1379@gmail.com>
2022-10-19 01:34:50 -0400

    case 1 finished

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,4 @@
-export const BASEURL = 'http://localhost:8080/api/';
-export const PDFURL = 'http://localhost:8080/PurchaseOrderPDF?repid=';
+// export const BASEURL = 'http://localhost:8080/api/';
+// export const PDFURL = 'http://localhost:8080/PurchaseOrderPDF?repid=';
+export const BASEURL = '/api/';
+export const PDFURL = '/PurchaseOrderPDF?repid='; 
