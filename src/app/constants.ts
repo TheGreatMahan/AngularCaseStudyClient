4dbcd597cfaa7d73f5ed16f1fa0ae3afa6ad52b8
Mahan <mahanmahdipour1379@gmail.com>
2022-11-29 20:04:37 -0500

    Lab 18 finished

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,4 @@
-export const BASEURL = 'http://localhost:8080/api/';
-export const PDFURL = 'http://localhost:8080/PurchaseOrderPDF?repid=';
-// export const BASEURL = '/api/';
-// export const PDFURL = '/PurchaseOrderPDF?repid='; 
+// export const BASEURL = 'http://localhost:8080/api/';
+// export const PDFURL = 'http://localhost:8080/PurchaseOrderPDF?repid=';
+export const BASEURL = '/api/';
+export const PDFURL = '/PurchaseOrderPDF?repid='; 
